819cf2525c904447a0cc712603215d5b21008250
Akshay R <akshayrajan345@gmail.com>
2024-07-30 22:35:02 +0530

    Hello world done!

--- MAD/helloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ MAD/helloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.helloworld;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +25,22 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            b1 = (Button) findViewById(R.id.b1);
+            b1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    AlertDialog.Builder alert = new AlertDialog.Builder(context);
+                    alert.setTitle("Hello, World!");
+
+                    AlertDialog myAlert = alert.create();
+                    myAlert.show();
+
+                }
+            });
+
             return insets;
         });
     }
+
 }
