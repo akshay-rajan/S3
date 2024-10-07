757a6e0708b4568c3fb82f7ee5d0b4cf6788106e
Akshay R <akshayrajan345@gmail.com>
2024-10-07 20:16:29 +0530

    Add Calculator app code

--- MAD/calculator/MainActivity.java
+++ MAD/calculator/MainActivity.java
@@ -1,115 +1,163 @@
-package com.example.simplecalculator;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.calculator;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView display;
-    Button equals, plus, minus, multiply, divide;
-    Button zero, one, two, three, four, five, six, seven, eight, nine;
-    Button clear, backspace;
-
-    String content = "";
-
+    String first = null;
+    String second = null;
+    String op = null;
+    Button add, subtract, multiply, divide, equals, clear, back, dot;
+    Button[] digits = new Button[10];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         display = (TextView) findViewById(R.id.display);
 
-        equals = (Button) findViewById(R.id.equals);
-        plus = (Button) findViewById(R.id.plus);
-        minus = (Button) findViewById(R.id.minus);
+        add = (Button) findViewById(R.id.add);
+        subtract = (Button) findViewById(R.id.subtract);
         multiply = (Button) findViewById(R.id.multiply);
+        divide = (Button) findViewById(R.id.divide);
+        Button[] operators = {add, subtract, multiply, divide};
 
-        zero = (Button) findViewById(R.id.zero);
-        one = (Button) findViewById(R.id.one);
-        two = (Button) findViewById(R.id.two);
-        three = (Button) findViewById(R.id.three);
-        four = (Button) findViewById(R.id.four);
-        five = (Button) findViewById(R.id.five);
-        six = (Button) findViewById(R.id.six);
-        seven = (Button) findViewById(R.id.seven);
-        eight = (Button) findViewById(R.id.eight);
-        nine = (Button) findViewById(R.id.nine);
+        equals = (Button) findViewById(R.id.equals);
+        clear = (Button) findViewById(R.id.clear);
+        back = (Button) findViewById(R.id.back);
 
-        zero.setOnClickListener(new View.OnClickListener() {
+        for (int i = 0; i < 10; i++) {
+            digits[i] = (Button) findViewById(getResources().getIdentifier("b" + i, "id", getPackageName()));
+        }
+
+        // Handle operator clicks
+        for (Button sign: operators) {
+            sign.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // If no entry in the display, do nothing. Otherwise
+                    if (first != null || second != null) {
+                        String operator = sign.getText().toString();
+                        if (first != null && second != null) {
+                            // Both numbers are set, hence perform the previously stored operation
+                            first = calculate(first, second, op);
+                            second = null;
+                            display.setText(first);
+                        } else if (first == null) {
+                            // If the second number is set, but not the first number
+                            first = second;
+                            second = null;
+                        }
+                        // If the first number is the only number set, store the operator
+                        op = operator;
+                    }
+                }
+            });
+        }
+
+        // Handle equals button
+        equals.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                content = content + "0";
-                display.setText(content);
+            public void onClick(View view) {
+                if (first == null || second == null) return;
+                first = calculate(first, second, op);
+                second = null;
+                op = null;
+                display.setText(first);
             }
         });
-        one.setOnClickListener(new View.OnClickListener() {
+
+        // Handle backspace
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                content = content + "1";
-                display.setText(content);
+            public void onClick(View view) {
+                if (first != null && second == null) {
+                    // If the first number is currently on display
+                    first = first.substring(0, first.length() - 1);
+                    display.setText(first);
+                } else if (first != null) {
+                    // If the second number is currently on display
+                    second = second.substring(0, second.length() - 1);
+                    display.setText(second);
+                }
             }
         });
-        two.setOnClickListener(new View.OnClickListener() {
+
+        // Clear the display and reset the calculator
+        clear.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                content = content + "2";
-                display.setText(content);
+            public void onClick(View view) {
+                first = null;
+                second = null;
+                op = null;
+                display.setText("0");
             }
         });
-        three.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                content = content + "3";
-                display.setText(content);
-            }
-        });
-        four.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                content = content + "4";
-                display.setText(content);
-            }
-        });
-        five.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                content = content + "5";
-                display.setText(content);
-            }
-        });
-        six.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                content = content + "6";
-                display.setText(content);
-            }
-        });
-        seven.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                content = content + "7";
-                display.setText(content);
-            }
-        });
-        eight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                content = content + "8";
-                display.setText(content);
-            }
-        });
-        nine.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                content = content + "9";
-                display.setText(content);
-            }
-        });
+
+        // For each digit button
+        for (Button b: digits) {
+            b.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Get the digit from the button
+                    String digit = ((Button) view).getText().toString();
+                    String curr = "0";
+                    // Append the digit to the current number in the display
+                    if (first == null) {
+                        first = digit;
+                        curr = first;
+                    } else if (second == null && op == null) {
+                        first += digit;
+                        curr = first;
+                    } else if (second == null) {
+                        second = digit;
+                        curr = second;
+                    } else {
+                        second += digit;
+                        curr = second;
+                    }
+                    display.setText(curr);
+                }
+            });
+        }
+    }
+    // Perform the calculation
+    public String calculate(String first, String second, String op) {
+        int f = Integer.parseInt(first);
+        int s = Integer.parseInt(second);
+
+        int ans = 0;
+        switch (op) {
+            case "+":
+                ans = f + s;
+                break;
+            case "-":
+                ans = f - s;
+                break;
+            case "X":
+                ans = f * s;
+                break;
+            case "/":
+                ans = f / s;
+                break;
+        }
+        return Integer.toString(ans);
     }
 }
